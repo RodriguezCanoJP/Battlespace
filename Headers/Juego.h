44abfb99bc35442d466e9b5595809bbb16ecabaf
JuanPablo <jp99@estudiantec.cr>
2023-04-05 14:08:35 -0600

    documentacion

--- Headers/Juego.h
+++ Headers/Juego.h
@@ -12,13 +12,15 @@
 class Juego {
 private:
     //Variables iniciales
-    sf::RenderWindow* window; //puntero a la ventana
-    sf::Event ev; //Ya esta inicializado
-    sf::VideoMode videoMode; //guarda las dimensiones de la ventana
-    Jugador jugador;
-    float velJugador;
-    int bulletQty;
-    std::vector <Bullet*> bullets;
+    sf::RenderWindow* window; ///< puntero a la ventana
+    sf::Event ev; ///< Ya esta inicializado
+    sf::VideoMode videoMode; ///< guarda las dimensiones de la ventana
+    Jugador jugador; ///< Instancia de la nave del jugador
+    float velJugador; ///< velocidad de la nave
+    int bullet_qty; ///< cantidad de balas disponibles
+    int bullet_vel; ///< velocidad de las balas
+    std::vector <Bullet*> bullets_disponibles; ///< vector con objetos Bullet disponibles
+    std::vector <Bullet*> bullets_usadas; ///< vector con objetos Bullet disparadas
 
 
     //Funciones privadas
@@ -30,11 +32,43 @@ public:
     ~Juego();
 
     //Funciones publicas
+    /**
+     * consulta si la ventana de juego sigue abierta
+     * @return bool
+     */
     const bool running() const;
+
+    /**
+     * consulta si hay eventos de ventana como presionar la x
+     */
     void pollEvents();
+
+    /**
+     * consulta si hay movimientos de teclado que corresponden a input de movimiento de la nave, llama
+     * a los metodos de jugador para actualizar la posicion
+     */
     void movJugador();
+
+    /**
+     * consulta is hay movimientos de teclado que corresponden a input de disparo de la nvae, llama
+     * a los metodos de Bullet guardado en el vector bullets_disponibles para comenzar su trayectoria
+     * y guarda el puntero de este en el vector bullets_usadas
+     */
     void disparo();
+
+    /**
+     * llama al operador delete de Bullet cuando la bala sale de la pantalla y actualisa bullets_usadas
+     */
+    void recicla_balas();
+
+    /**
+     * llama a las otras funciones que son recurrentes a traves del juego
+     */
     void update();
+
+    /**
+     * dibuja los objetos en la ventana
+     */
     void render();
 
 };
