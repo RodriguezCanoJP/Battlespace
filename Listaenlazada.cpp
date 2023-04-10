aa70a0c8a5e07e4d1ac5b89d555c4f784d459446
Yitzy <jimenezyitzy17@gmail.com>
2023-04-10 17:56:01 -0600

    listaenlazada

---
+++ Listaenlazada.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Juan Pablo Rodriguez on 10/4/23.
+//
+
+#include "Listaenlazada.h"
---
+++ Listaenlazada.h
@@ -0,0 +1,27 @@
+//
+// Created by yitzy on 07/04/23.
+//
+
+#ifndef UNTITLED_LISTAENLAZADA_H
+#define UNTITLED_LISTAENLAZADA_H
+#include "Enemigo.h"
+
+class ListaEnlazada {
+private:
+    Enemigo* head;
+    int size;
+public:
+    ///Constructor/Destructor
+    ListaEnlazada();
+    ~ListaEnlazada();
+
+    Enemigo* getNave();
+    void insertar(Enemigo* nave);
+    void eliminar(Enemigo* nave);
+    int ssize();
+    bool isEmpty();
+
+};
+
+
+#endif //UNTITLED_LISTAENLAZADA_H
