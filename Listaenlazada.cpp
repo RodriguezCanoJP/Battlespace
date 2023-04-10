ed90815463084ea3077ef88fc22b8de32dd43457
Yitzy <jimenezyitzy17@gmail.com>
2023-04-10 17:59:13 -0600

    listaenlazada2

--- Listaenlazada.cpp
+++ Listaenlazada.cpp
@@ -1,5 +1,66 @@
 //
-// Created by Juan Pablo Rodriguez on 10/4/23.
+// Created by yitzy on 07/04/23.
 //
 
-#include "Listaenlazada.h"
+#include "Headers/ListaEnlazada.h"
+#include "Headers/Enemigo.h"
+#include <iostream>
+
+ListaEnlazada::ListaEnlazada(){
+    this->head = nullptr;
+};
+
+ListaEnlazada::~ListaEnlazada() {
+    for(int i = 0; i < size; i++){
+        eliminar(this->head);
+    }
+};
+
+Enemigo *ListaEnlazada::getNave() {
+    return head;
+}
+
+void ListaEnlazada::insertar(Enemigo* nave) {
+    if(this->head == nullptr){
+        this->head = nave;
+    }else {
+        Enemigo *temp = this->head;
+        nave->setNext(temp);
+        this->head = nave;
+        temp->setPrevious(nave);
+        this->size++;
+    }
+}
+
+void ListaEnlazada::eliminar(Enemigo* nave) {
+    if(this->isEmpty()){
+        std::cout << "Lista vacia" << "\n";
+    }else{
+        Enemigo* target = this->head;
+        for(int i = 0; i < size; i++){
+            if(target = nave){
+                break;
+            }else{
+                target = target->getNext();
+            }
+        }
+        target->getNext()->setPrevious(target->getPrevious());
+        target->getPrevious()->setNext(target->getNext());
+        delete target;
+        this->size --;
+    }
+
+}
+
+bool ListaEnlazada::isEmpty(){
+    return this->size == 0;
+}
+
+int ListaEnlazada::ssize() {
+    return this->size;
+}
+
+
+
+
+
